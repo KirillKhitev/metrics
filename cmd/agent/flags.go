83d3951d58264a87d2f792e63069773f45033b8d
Kirill Khitev <nomer_47@mail.ru>
2023-12-11 23:17:03 +0500

    iter5: переменные окружения в агенте и сервере

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 var flagAddrRun string
@@ -13,4 +17,24 @@ func parseFlags() {
 	flag.IntVar(&flagPollInterval, "p", 2, "poll metrics interval")
 	flag.IntVar(&flagReportInterval, "r", 10, "send metrics report interval")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagAddrRun = envRunAddr
+	}
+
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		if val, err := strconv.Atoi(envPollInterval); err == nil {
+			flagPollInterval = val
+		} else {
+			log.Println(fmt.Sprintf("wrong value environment POLL_INTERVAL: %s", envPollInterval))
+		}
+	}
+
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		if val, err := strconv.Atoi(envReportInterval); err == nil {
+			flagReportInterval = val
+		} else {
+			log.Println(fmt.Sprintf("wrong value environment REPORT_INTERVAL: %s", flagReportInterval))
+		}
+	}
 }
